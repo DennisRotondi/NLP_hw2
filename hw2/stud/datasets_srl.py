6e447dd0368a939772916840a566b0af90b3bf68
dennisrotondi <dennisrotondi6@gmail.com>
2022-07-10 23:12:58 +0200

    a new day of work

--- hw2/stud/datasets_srl.py
+++ hw2/stud/datasets_srl.py
@@ -1,12 +1,57 @@
 import pytorch_lightning as pl
+import torch
 from torch.utils.data import DataLoader, Dataset
 from typing import Tuple, List, Any, Dict
-
+from torch.nn.utils.rnn import pad_sequence
+from transformers import AutoTokenizer
+import numpy as np
 class Dataset_SRL_34(Dataset):
+    def __init__(self, sentences: Dict[str, List[str]], need_train: bool):
+        # if the dataset is for a model that need_train we assume to have labels
+        self.has_labels = need_train 
+        if self.has_labels:
+            # with this function we create a dict to encode and decode easily labels of roles
+            self.create_labels_id_mapping_roles()
+        self.data = self.make_data(sentences)
 
+    def create_labels_id_mapping_roles(self):
+        # these labels have been extracted studying the dataset from the notebook
+        labels = ['agent', 'theme', 'beneficiary', 'patient', 'topic', 'goal', 'recipient', 
+            'co-theme', 'result', 'stimulus', 'experiencer', 'destination', 'value', 'attribute', 
+            'location', 'source', 'cause', 'co-agent', 'time', 'co-patient', 'product', 'purpose', 
+            'instrument', 'extent', 'asset', 'material', '_']
+        self.labels_to_id = {lab: i for i, lab in enumerate(labels)}
+        self.id_to_labels = {i: lab for i, lab in enumerate(labels)}
 
-    
-    pass
+    def make_data(self, sentences):
+        data = list() 
+        for j, ids in enumerate(sentences):
+            # we extract the position of the predicates
+            predicate_positions = [i for i, p in enumerate(sentences[ids]["predicates"]) if p != '_' and p != 0]
+            sentence = sentences[ids]["lemmas"] 
+            s_length = len(sentence)
+            for predicate_position in predicate_positions:
+                item = dict()
+                item["id"] = j
+                item["len"] = s_length
+
+                # here I build a tuple to attain the same input as proposed by "Shi - Lin 19" after the embedding
+                # I have also added the predicate disambiguation of that value to check if it's possible to improve the result.
+                item["input"] = (sentence, [sentence[predicate_position],sentences[ids]["predicates"][predicate_position]])
+                if self.has_labels:
+                    # the desired output are the labels already encoded for each role associated to the sentence[predicate_position]
+                    item["role_labels"] = torch.as_tensor([self.labels_to_id[i] for i in sentences[ids]["roles"][predicate_position]])
+                    # note that input and output have different sizes after the input embedding if a word_piece tokenizer is used
+                    # (as in my case), but don't worry, the model will produce an average mean of piece_tokens so that the sizes
+                    # will be compatible.
+                data.append(item)               
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
 
 # class Dataset_SRL_234(dataset_srl_34):
 #     pass
@@ -15,7 +60,7 @@ class Dataset_SRL_34(Dataset):
 #     pass
 
 class SRL_DataModule(pl.LightningDataModule):
-    def __init__(self, hparams: object, task: str, sentences: Dict[str, List[str]], sentences_test: Dict[str, List[str]]=None) -> None:
+    def __init__(self, hparams: dict, task: str, sentences: Dict[str, List[str]], sentences_test: Dict[str, List[str]] = None) -> None:
         super().__init__()
         self.save_hyperparameters(hparams)
         self.sentences = sentences
@@ -30,17 +75,19 @@ class SRL_DataModule(pl.LightningDataModule):
         #     DATASET = Dataset_SRL_234
         # else:
         #     DATASET = Dataset_SRL_1234
-        self.data_train = DATASET(self.sentences)
+        self.data_train = DATASET(self.sentences, self.hparams.need_train)
         if self.sentences_test: 
-            self.data_test = DATASET(self.sentences_test)
-    #NEED TO ADD COLLATEfn
+            self.data_test = DATASET(self.sentences_test, self.hparams.need_train)
+        # now we free up space
+        delattr(self, "sentences")
+        delattr(self, "sentences_test")
+
     def train_dataloader(self):
         return DataLoader(
                 self.data_train, 
                 batch_size = self.hparams.batch_size, 
-                shuffle = True,
+                shuffle = self.hparams.need_train, # if we need train we shuffle it
                 num_workers = self.hparams.n_cpu,
-                pin_memory = True,
                 collate_fn = self.collate_fn,
                 persistent_workers = True
             )
@@ -51,44 +98,47 @@ class SRL_DataModule(pl.LightningDataModule):
                 batch_size = self.hparams.batch_size, 
                 shuffle = False,
                 num_workers = self.hparams.n_cpu,
-                pin_memory = True,
                 collate_fn = self.collate_fn,
                 persistent_workers = True
             )
-    # here we define our collate function
-    # from transform notebook
-    def collate_fn(batch) -> Dict[str, torch.Tensor]:
-        batch_out = tokenizer(
-            [sentence["tokens"] for sentence in batch],
+    # here we define our collate function to apply the padding
+    # ispiration from transformers notebook
+    def collate_fn(self, batch) -> Dict[str, torch.Tensor]:
+        tokenizer = AutoTokenizer.from_pretrained(self.hparams.language_model_name)
+        batch_out = tokenizer.batch_encode_plus(
+            [sentence["input"] for sentence in batch],
             return_tensors="pt",
             padding=True,
             # We use this argument because the texts in our dataset are lists of words.
             is_split_into_words=True,
         )
-        labels = []
-        ner_tags = [sentence["ner_tags"] for sentence in batch]
-        for i, label in enumerate(ner_tags):
-        # obtains the word_ids of the i-th sentence
-        word_ids = batch_out.word_ids(batch_index=i)
-        previous_word_idx = None
-        label_ids = []
-        for word_idx in word_ids:
-            # Special tokens have a word id that is None. We set the label to -100 so they are automatically
-            # ignored in the loss function.
-            if word_idx is None:
-            label_ids.append(-100)
-            # We set the label for the first token of each word.
-            elif word_idx != previous_word_idx:
-            label_ids.append(label[word_idx])
-            # For the other tokens in a word, we set the label to -100 so they are automatically
-            # ignored in the loss function.
-            else:
-            label_ids.append(-100)
-            previous_word_idx = word_idx
-        labels.append(label_ids)
-        
-        # pad the labels with -100
-        batch_max_length = len(max(labels, key=len))
-        labels = [l + ([-100] * abs(batch_max_length - len(l))) for l in labels]
+        ids = list()
+        word_ids = list()
+        labels = list()
+        lens = list()
+        for i, sentence in enumerate(batch):
+            ids.append(sentence["id"])
+            lens.append(sentence["len"])
+
+            w_id = np.array(batch_out.word_ids(batch_index=i), dtype=np.float64)
+            # since w_id contains None values we want to remove them to have the conversion in tensor
+            # we'll do so by creating a "special_token index" that is +1 higher than the last word token
+            special_idx = np.nanmax(w_id) + 1
+            w_id[np.isnan(w_id)] = special_idx
+            # we need to mask vectors belonging to the "second" sentence (the one made to produce the embedding) 
+            # they all will get a value different from all the other tokens so it will be easy to remove
+            w_id[batch_out["token_type_ids"][i]] = special_idx
+            word_ids.append(w_id)
+            if self.hparams.need_train:
+                labels.append(sentence["role_labels"])
+
+        labels = pad_sequence(
+                [torch.as_tensor(label) for label in labels],
+                batch_first=True,
+                padding_value=-100
+            )
+        batch_out["id"] = torch.as_tensor(ids)
+        # np conversion of the list to speedup the tensor creation
+        batch_out["word_id"] = torch.as_tensor(np.array(word_ids), dtype=torch.long) 
         batch_out["labels"] = torch.as_tensor(labels)
         return batch_out
--- hw2/stud/implementation.py
+++ hw2/stud/implementation.py
@@ -1,10 +1,17 @@
 import json
 import random
-
 import numpy as np
-from typing import List, Tuple
-
+from typing import List, Tuple, Dict
 from model import Model
+import dataclasses
+from dataclasses import dataclass, asdict
+from transformers import AutoModel
+from transformers_embedder.embedder import TransformersEmbedder
+import torch
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch import nn, optim
+import torch.nn.functional as F
+import pytorch_lightning as pl
 
 
 def build_model_34(language: str, device: str) -> Model:
@@ -51,6 +58,23 @@ def build_model_1234(language: str, device: str) -> Model:
     """
     raise NotImplementedError
 
+@dataclass
+class HParams():
+    # dataset stuff
+    need_train: bool = True
+    batch_size: int = 3
+    n_cpu: int = 8
+    # model stuff
+    language_model_name: str = "bert-base-uncased"
+    lr: int = 1e-3
+    wd: int = 1e-5
+    embedding_dim: int = 768
+    hidden_dim: int = 512
+    bidirectional: bool = True 
+    num_layers: int = 5
+    dropout: float = 0.4
+    trainable_embeddings: bool = True 
+    role_classes: int = 27 # number of different SRL roles for this homework
 
 class Baseline(Model):
     """
@@ -190,3 +214,70 @@ class StudentModel(Model):
                     }
         """
         pass
+
+
+class SRL_34(pl.LightningModule):
+    def __init__(self, hparams: dict) -> None:
+        super(SRL_34, self).__init__()
+        self.save_hyperparameters(hparams)
+        self.transformer_model = AutoModel.from_pretrained("bert-base-uncased", output_hidden_states=True)
+        if not self.hparams.trainable_embeddings:
+            for param in self.transformer_model.parameters():
+                param.requires_grad = False
+        self.lstm = nn.LSTM(self.hparams.embedding_dim, self.hparams.hidden_dim, 
+                            bidirectional = self.hparams.bidirectional,
+                            num_layers = self.hparams.num_layers, 
+                            dropout = self.hparams.dropout if self.hparams.num_layers > 1 else 0,
+                            batch_first = True,
+                            )
+        lstm_output_dim = self.hparams.hidden_dim if self.hparams.bidirectional is False else self.hparams.hidden_dim * 2
+        self.dropout = nn.Dropout(self.hparams.dropout)
+        self.classifier = nn.Linear(lstm_output_dim, self.hparams.role_classes)
+        ##### ---- #### ---- #### ---- ####
+
+    def forward(self, x: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        model_kwargs = {
+          "input_ids": x["input_ids"], 
+          "attention_mask": x["attention_mask"],
+          "token_type_ids": x["token_type_ids"]
+        }
+        # if reduction sum for example
+        transformers_outputs = self.transformer_model(**model_kwargs)
+        transformers_outputs_sum = torch.stack(transformers_outputs.hidden_states[-4:], dim=0).sum(dim=0)
+        # I use the Riccardo Orlando merger to average the tokens of the initial word splitted by the tokenizer
+        embeddings = TransformersEmbedder.merge_scatter(transformers_outputs_sum, x["word_id"])[:,:-1,:]
+        o, (h, c) = self.lstm(embeddings)
+        o = self.dropout(o)
+        return self.classifier(o)
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=self.hparams.lr, betas=(0.9, 0.999), eps=1e-6, weight_decay=self.hparams.wd)
+        reduce_lr_on_plateau = ReduceLROnPlateau(optimizer, mode='min',verbose=True, min_lr=1e-8)
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": reduce_lr_on_plateau,
+                "monitor": 'loss',
+                "frequency": 1
+            },
+        }
+
+    def loss_function(self, input, target):
+        CE = F.CrossEntropyLoss(input, target, ignore_index = -100)
+        return {"loss": CE}
+
+    def training_step(self, batch: Dict[str, torch.Tensor], batch_idx) -> Dict[str, torch.Tensor]:
+        output = self(batch)
+        loss = self.loss_function(output, batch["labels"])
+        self.log_dict(loss)
+        return loss['loss']
+
+    def validation_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict[str, torch.Tensor]:
+        output = self(batch)
+        loss = self.loss_function(output, batch["labels"])
+        return {"loss_vae_val": loss['loss']}
+
+    def validation_epoch_end(self, outputs: List[Dict[str, torch.Tensor]]):
+        avg_loss = torch.stack([x["loss_vae_val"] for x in outputs]).mean()
+        self.log_dict({"avg_val_loss_vae": avg_loss})
+        return {"avg_val_loss_vae": avg_loss}
